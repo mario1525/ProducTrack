ee3c295a338708b6ce3651fd170272d8df558cb0
mario1525 <mbeltran4892@gmail.com>
2024-08-06 08:46:37 -0500

    add: docker api-gateway

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- api-gateway/src/auth/auth.controller.ts
+++ api-gateway/src/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { map } from 'rxjs/operators';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly httpService: HttpService) {}
+
+  @Post('login')
+  login(@Body() body) {
+    // Redirigir la solicitud de inicio de sesión al servicio de autenticación
+    return this.httpService
+      .post('http://auth-service/login', body)
+      .pipe(map((response) => response.data));
+  }
+
+  @Post('register')
+  register(@Body() body) {
+    // Redirigir la solicitud de registro al servicio de autenticación
+    return this.httpService
+      .post('http://auth-service/register', body)
+      .pipe(map((response) => response.data));
+  }
+}
